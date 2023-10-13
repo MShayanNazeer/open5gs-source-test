11f97f9a472b1b2fac15186a7b0aa629b949f1ee
Shayan Nazeer <62965848+SHAYAN3519@users.noreply.github.com>
2023-10-13 01:35:59 -0400

    Update open5gs.sh

--- scripts/open5gs.sh
+++ scripts/open5gs.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
-set -ex
-BINDIR=`dirname $0`
-source $BINDIR/common.sh
+#set -x
 
-sudo sysctl -w net.ipv4.ip_forward=1
-sudo iptables -t nat -A POSTROUTING -s 10.45.0.0/16 ! -o ogstun -j MASQUERADE
+# load configs 
+source /local/repository/scripts/setup-config
+
+### Enable IPv4/IPv6 Forwarding
+sysctl -w net.ipv4.ip_forward=1
+sysctl -w net.ipv6.conf.all.forwarding=1
+sysctl -w net.ipv4.ip_nonlocal_bind=1
+
+# sudo iptables -t nat -A POSTROUTING -s 10.45.0.0/16 ! -o ogstun -j MASQUERADE
 
 if [ -f $SRCDIR/open5gs-setup-complete ]; then
     echo "setup already ran; not running again"
